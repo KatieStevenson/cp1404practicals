1a4c9a777444c9593e502f97e2fa2d9fabf35658
KatieStevenson <katie.stevenson@my.jcu.edu.au>
2020-03-15 20:50:31 +1000

    Update broken_score.py with generate_random_number function.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,21 +1,34 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+Should ask the user for their score and print the result.
+Write a new function that takes in the user's score as a parameter and returns the result to be printed.
+The function should not print it.
 """
 
-# TODO: Fix this!
+import random
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score > 50:
-    print("Passable")
-elif score > 90:
-    print("Excellent")
-elif score < 50:
-    print("Bad")
 
-# Changed if statements to ELIF as I would enter a score greater than 100.
-# It would print "Invalid score", "Passable", "Excellent" and "Bad" etc.
+def generate_random_number():
+    score = random.randint(0, 100)
+    return score, determine_score(score)
+
+
+def determine_score(score):
+    if score < 0:
+        return "Invalid Score"
+    elif score > 100:
+        return "Invalid Score"
+    elif score > 50:
+        return "Passable"
+    elif score > 90:
+        return "Excellent"
+    elif score < 50:
+        return "Bad"
+
+
+def main():
+    # score = float(input("Enter score: ")) # COMMENT THIS OUT AS RANDOM NUMBER IS REPLACING USER INPUT
+
+    print(generate_random_number())
+
+
+main()
