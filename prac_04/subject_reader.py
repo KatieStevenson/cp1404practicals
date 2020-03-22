eecfb71c2204fce56591ac36d6c7d93719124b70
KatieStevenson <61174796+KatieStevenson@users.noreply.github.com>
2020-03-22 21:40:42 +1000

    Upload completed subject_reader.py file.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -15,15 +15,12 @@ def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        list_parts = line.split(",")
+        list_parts[2] = int(list_parts[2])
+        print(list_parts, end = ",") # Don't know why 'None' is at the end of the print output.
+
     input_file.close()
 
 
 main()
+
